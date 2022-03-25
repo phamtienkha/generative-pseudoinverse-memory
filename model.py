a96ab01c41c1dfc5e468da12ecc0ad099da5b33f
phamtienkha <16438545+phamtienkha@users.noreply.github.com>
2022-03-25 18:47:35 +0700

    Update model.py

--- model.py
+++ model.py
@@ -69,6 +69,75 @@ class Model(nn.Module):
         bijective_loss = -torch.mean(torch.sum(input * torch.log(input_recon + 1e-12), dim=1) +
                                      torch.sum((1 - input) * torch.log(1 - input_recon + 1e-12), dim=1))
         return bijective_loss
+    
+        def denoise(self, input, input_noise, episode_size, iterative_step):
+        batch_size = input.shape[0] // episode_size
+        input_encoded = self.encode(input)
+        input_encoded = input_encoded.reshape(episode_size, batch_size, self._code_size)
+
+        input_encoded_noise = self.encode(input_noise)
+        input_encoded_noise = input_encoded_noise.reshape(episode_size, batch_size, self._code_size)
+
+        posterior_memory, dkl_M = self.write(input_encoded=input_encoded)
+
+        input_recon_list = []
+        z, dkl_w = self.read(input_encoded_noise, posterior_memory)
+        z = z.reshape(episode_size * batch_size, self._code_size)
+        input_recon = self.decode(z.reshape(episode_size * batch_size, self._code_size))
+        input_recon = self._binarize(input=input_recon, threshold=0.1)
+        input_recon_list.append(input_recon)
+
+        for _ in range(iterative_step - 1):
+            input_recon_encoded = self.encode(input_recon)
+            input_recon_encoded = input_recon_encoded.reshape(episode_size, batch_size, self._code_size)
+
+            z, dkl_w = self.read(input_recon_encoded, posterior_memory)
+            z = z.reshape(episode_size * batch_size, self._code_size)
+            input_recon = self.decode(z.reshape(episode_size * batch_size, self._code_size))
+            input_recon = self._binarize(input=input_recon, threshold=0.1)
+            input_recon_list.append(input_recon)
+
+        input_recon = input_recon_list[-1]
+        recon_loss = -torch.mean(torch.sum(input * torch.log(input_recon + 1e-12), dim=1) +
+                                 torch.sum((1 - input) * torch.log(1 - input_recon + 1e-12), dim=1))
+        
+        return input_recon_list
+
+    def generate(self, input, episode_size, sample_size, iterative_step, threshold=0.3):
+        batch_size = input.shape[0] // episode_size
+        input_encoded = self.encode(input)
+        input_encoded = input_encoded.reshape(episode_size, batch_size, self._code_size)
+
+        posterior_memory, dkl_M = self.write(input_encoded=input_encoded)
+        M = posterior_memory[0]
+        w = torch.randn(sample_size, batch_size, self._memory_size)
+        z = torch.bmm(w.transpose(0, 1), M).transpose(0, 1)
+        z = z.reshape(sample_size * batch_size, self._code_size)
+
+        sample_list = []
+        sample = self.decode(z.reshape(sample_size * batch_size, self._code_size))
+        sample = self._binarize(input=sample, threshold=0.05)
+        sample_list.append(sample)
+
+        for _ in range(iterative_step - 1):
+            sample_encoded = self.encode(sample)
+            sample_encoded = sample_encoded.reshape(sample_size, batch_size, self._code_size)
+
+            z, dkl_w = self.read(sample_encoded, posterior_memory)
+            z = z.reshape(sample_size * batch_size, self._code_size)
+            sample = self.decode(z.reshape(sample_size * batch_size, self._code_size))
+            sample = self._binarize(input=sample, threshold=threshold)
+            sample_list.append(sample)
+
+        return sample_list
+
+    def _binarize(self, input, threshold=0.3):
+        input_binarized = torch.zeros_like(input)
+        for i in range(input.shape[0]):
+            for j in range(input.shape[1]):
+                if input[i, j] >= threshold:
+                    input_binarized[i, j] = 1
+        return input_binarized
 
 
 
